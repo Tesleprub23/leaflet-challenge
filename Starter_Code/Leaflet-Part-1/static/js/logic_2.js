1bf4ab5037be82e63addff5c8bc913ec5edc69e5
Tesleprub23 <tjlepasana@gmail.com>
2024-02-12 13:42:51 -0500

    Module 15

--- Starter_Code/Leaflet-Part-1/static/js/logic_2.js
+++ Starter_Code/Leaflet-Part-1/static/js/logic_2.js
@@ -7,7 +7,7 @@ let myMap = L.map("map", {
     zoom: 3,
     layers: [streets]
 });
-
+streets.addTo(myMap)
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 d3.json(url).then(function (data){
@@ -36,20 +36,47 @@ d3.json(url).then(function (data){
         }
         return magnitude * 4
     }
-    // L.geoJson (data, {
-    //     pointToLayer: function(feature, latitude){
-    //         return L.circleMarker(latitude)
-    //     },
+    L.geoJson (data, {
+        pointToLayer: function(feature, latitude){
+            return L.circleMarker(latitude);
+        },
     
-    // style: styleInfo,
-    // onEachFeature: function(feature, layer) { 
-    //     layer.bindPopup( 
-    //         "Magnitude:"
-    //         + feature.properties.mag
-    //         + "<br>Depth: " 
-    //         + feature.geometry.coordinates[2] 
-    //         + "<br>Location: " 
-    //         + feature.properties.place 
-    //     );    
-    // }}
-).addTo(map);
+    style: styleInfo,
+    onEachFeature: function(feature, layer) { 
+        layer.bindPopup( 
+            "Magnitude:"
+            + feature.properties.mag
+            + "<br>Depth: " 
+            + feature.geometry.coordinates[2] 
+            + "<br>Location: " 
+            + feature.properties.place 
+        );   
+    }}).addTo(myMap);
+
+
+// Set up the legend.
+let legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  let labels = [];
+  let grades = [-10, 10, 30, 50, 70, 90];
+  let colors = [
+    "#98EE00",
+    "#D4EE00",
+    "#EECC00",
+    "#EE9C00",
+    "#EA822C",
+    "#EA2C2C"
+
+  ];
+
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+        + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+};
+
+      legend.addTo(myMap);
+
+});
